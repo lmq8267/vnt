cac2638024c0eb8f8e523cac81942a74ea3ff8eb
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-08-30 08:48:16 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -22,7 +22,6 @@ https://mirror.ghproxy.com/
 https://ghproxy.net/
 https://dl.cnqq.cloudns.ch/
 "
-[ ! -f /usr/lib/lua/luci/model/cbi/vnt.lua ] && echo -e "\033[31m此脚本只适合更新已安装luci-app-vnt的程序！ \033[0m" ; exit 0
 
 log () {
    echo -e "\033[36;1m【$(TZ=UTC-8 date -R +%Y年%m月%d月\ %X)】 : \033[0m\033[35;1m$1 \033[0m"
@@ -32,10 +31,18 @@ log () {
    if [ "$2" = "vnts" ] ; then
    echo "【$(TZ=UTC-8 date -R +%Y年%m月%d月\ %X)】 : $1 " >>/tmp/vnts_update
    fi
+   if [ "$2" = "luci" ] ; then
+   echo "【$(TZ=UTC-8 date -R +%Y年%m月%d月\ %X)】 : $1 " >>/tmp/vnt_luci
+   fi
+}
+
+check () {
+   [ ! -f /usr/lib/lua/luci/model/cbi/vnt.lua ] && echo -e "\033[31m此脚本只适合更新已安装luci-app-vnt的程序！ \033[0m" ; exit 0
 }
 
 vnt () {
    echo "" >/tmp/vnt-cli_update
+   check
    ver="$($vnt_cli -h | grep version | awk -F ':' {'print $2'})"
    log "开始更新vnt-cli客户端程序..." vnt
    if [ -z "$1" ] ; then
@@ -54,6 +61,8 @@ vnt () {
        if [ ! -z "$ver" ] && [ ! -z "$tag" ] ; then
            if [ "$ver"x = "$tag"x ] ; then
                log "当前版本 ${ver} 已是最新版本，无需更新！" vnt && exit 0
+	   else
+	       log "当前版本 ${ver} 最新版本 ${tag} " vnt
 	   fi
        fi
        log "当前CPU架构 ${cpucore} 获取到最新版本vnt-${cpucore}-unknown-linux-musl-${tag}.tar.gz" vnt
@@ -78,7 +87,7 @@ vnt () {
    fi
    log "开始下载https://github.com/vnt-dev/vnt/releases/download/${tag}/vnt-${url}-${tag}.tar.gz" vnt 
    for proxy in $proxys ; do
-    curl -Lkso /tmp/vnt-cli.tar.gz "${proxy}https://github.com/vnt-dev/vnt/releases/download/${tag}/vnt-${url}-${tag}.tar.gz"
+    curl -Lkso /tmp/vnt-cli.tar.gz "${proxy}https://github.com/vnt-dev/vnt/releases/download/${tag}/vnt-${url}-${tag}.tar.gz" || wget --no-check-certificate -q -O /tmp/vnt-cli.tar.gz "${proxy}https://github.com/vnt-dev/vnt/releases/download/${tag}/vnt-${url}-${tag}.tar.gz"
     if [ "$?" = 0 ] ; then
         log "下载完成，开始解压/tmp/vnt-cli.tar.gz到/tmp/目录里..." vnt 
 	rm /tmp/vnt-cli >/dev/null 2>&1
@@ -104,6 +113,7 @@ exit 0
 
 vnts () {
    echo "" >/tmp/vnts_update
+   check
     ver="$($vnts -V | awk '{print $2}')"
    log "开始更新vnts服务端程序..." vnts
    if [ -z "$1" ] ; then
@@ -122,6 +132,8 @@ vnts () {
        if [ ! -z "$ver" ] && [ ! -z "$tag" ] ; then
            if [ "$ver"x = "$tag"x ] ; then
                log "当前版本 ${ver} 已是最新版本，无需更新！" vnts && exit 0
+	   else
+	       log "当前版本 ${ver} 最新版本 ${tag} " vnts
 	   fi
        fi
        log "当前CPU架构 ${cpucore} 获取到最新版本vnts-${cpucore}-unknown-linux-musl-${tag}.tar.gz" vnts
@@ -146,7 +158,7 @@ vnts () {
    fi
    log "开始下载https://github.com/vnt-dev/vnts/releases/download/${tag}/vnts-${url}-${tag}.tar.gz" vnts
    for proxy in $proxys ; do
-    curl -Lkso /tmp/vnts.tar.gz "${proxy}https://github.com/vnt-dev/vnts/releases/download/${tag}/vnts-${url}-${tag}.tar.gz"
+    curl -Lkso /tmp/vnts.tar.gz "${proxy}https://github.com/vnt-dev/vnts/releases/download/${tag}/vnts-${url}-${tag}.tar.gz" || wget --no-check-certificate -q -O /tmp/vnts.tar.gz "${proxy}https://github.com/vnt-dev/vnts/releases/download/${tag}/vnts-${url}-${tag}.tar.gz"
     if [ "$?" = 0 ] ; then
         log "下载完成，开始解压/tmp/vnts.tar.gz到/tmp/目录里..." vnts 
 	rm /tmp/vnts >/dev/null 2>&1
@@ -170,6 +182,46 @@ vnts () {
 exit 0
 }
 
+luci () {
+   echo "" >/tmp/vnt_luci
+   ver="$(opkg info luci-app-vnt | awk '/Version:/ {print $2}')"
+   [ -z "$ver" ] &&  log "无法获取本设备的luci-app-vnt版本号，退出更新！" luci && exit 0
+   log "开始更新luci-app-vnt ..." luci
+   if [ -z "$1" ] ; then
+   if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
+       tag="$( wget -T 5 -t 3 --user-agent "$user_agent" --max-redirect=0 --output-document=-  https://api.github.com/repos/lmq8267/luci-app-vnt/releases/latest  2>&1 | grep 'tag_name' | cut -d\" -f4 )"
+       [ -z "$tag" ] && tag="$( wget -T 5 -t 3 --user-agent "$user_agent" --quiet --output-document=-  https://api.github.com/repos/lmq8267/luci-app-vnt/releases/latest  2>&1 | grep 'tag_name' | cut -d\" -f4 )"
+   else
+       tag="$( curl --connect-timeout 3 --user-agent "$user_agent"  https://api.github.com/repos/lmq8267/luci-app-vnt/releases/latest  2>&1 | grep 'tag_name' | cut -d\" -f4 )"
+       [ -z "$tag" ] && tag="$( curl -L --connect-timeout 3 --user-agent "$user_agent" -s  https://api.github.com/repos/lmq8267/luci-app-vnt/releases/latest  2>&1 | grep 'tag_name' | cut -d\" -f4 )"
+   fi
+   else
+   tag="$1"
+   fi
+   [ -z "$tag" ] &&  log "无法从github获取到最新版本，退出更新！" luci && exit 0
+   if [ "$ver"x != "$tag"x ] ; then
+       log "当前版本 ${ver} 最新版本 ${tag} " luci
+       log "开始下载https://github.com/lmq8267/luci-app-vnt/releases/download/${tag}/luci-app-vnt_all.ipk" luci
+       for proxy in $proxys ; do
+           curl -Lkso /tmp/luci-app-vnt.ipk "${proxy}https://github.com/lmq8267/luci-app-vnt/releases/download/${tag}/luci-app-vnt_all.ipk" || wget --no-check-certificate -q -O /tmp/luci-app-vnt.ipk "${proxy}https://github.com/lmq8267/luci-app-vnt/releases/download/${tag}/luci-app-vnt_all.ipk"
+           if [ "$?" = 0 ] ; then
+               log "下载完成，开始更新 luci-app-vnt ..." luci
+	       rm /var/lock/opkg.lock >/dev/null 2>&1
+	       opkg install --force-reinstall --force-overwrite --force-depends /tmp/luci-app-vnt.ipk >/dev/null 2>&1
+               ver="$(opkg info luci-app-vnt | awk '/Version:/ {print $2}')"
+	    if [ "$ver"x = "$tag"x ] ; then
+	        log "luci-app-vnt  ${ver}更新${tag}成功！" luci
+                log "请退出登录路由器管理页面，重新登录，进入VNT配置页面修改配置后重新启动！" luci
+                break
+	    else
+	        log "更新失败！" luci
+	    fi
+         fi
+      done
+   fi
+exit 0
+}
+
 case $1 in
 vnt-cli)
         vnt $2
@@ -179,6 +231,9 @@ vnt)
 	;;
 vnts) 
 	vnts $2
+	;;
+luci) 
+	luci $2
 	;;
 *)
         exit 0
